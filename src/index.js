805b30dea9da5736bccbf0d5ce6aacfffbfffd4f
Himanshu Sharma <himanshusharma0076@gmail.com>
2025-06-07 23:23:20 +0530

    advace routing and dynamic params, query

--- src/index.js
+++ src/index.js
@@ -28,3 +28,50 @@ app.post('/users', (req, res) => {
 app.delete('/users/:userId', (req, res) => {
     res.send('User delete successfully!')
 })
+
+// ADVANCED ROUTING TECHNICS
+
+//***** */ the '?' in front of b means b is option we can get the data with the route 'ac'  *****
+
+app.get("/ab?c", (req, res) => {
+    res.send({ 'name': "Himanshu", 'last_name': "Sharma" })
+})
+
+//***** */ the '+' in front of b means we can unlimited b in the route like - abbbbbbbbbbbbbbbbc (but c must me at the end)  *****
+app.get("/ab+c", (req, res) => {
+    res.send({ 'name': "Himanshu", 'last_name': "Sharma" })
+})
+
+//***** */ the * means you can add anything between the ab and cd like abHIMANSHUcd (but it most follow the pattern start with ab and end with cd)  *****
+app.get("/ab*cd", (req, res) => {
+    res.send({ 'name': "Himanshu", 'last_name': "Sharma" })
+})
+
+// THIS BC IS OPTIONAL and similary for the * and +
+app.get("/a(bc)?d", (req, res) => {
+    res.send({ 'name': "Himanshu", 'last_name': "Sharma" })
+})
+
+// THIS FOR THE REGES EXPRESSION 
+app.get("/a/", (req, res) => {
+    res.send({ 'name': "Himanshu", 'last_name': "Sharma" })
+})  // this means it will work for all route which contains a letter like - cab, aam, apple etc
+
+app.get("/.fly$/", (req, res) => {
+    res.send({ 'name': "Himanshu", 'last_name': "Sharma" })
+})  // this means it will work for all the routes that contains fly at the end like - butterfly, dragonfly , fly etc
+
+// THE above things are not used in real world it just for knowledge purpose 
+
+
+// HOW TO GET THE QUERY 
+app.get('/users', (req, res) => {
+    console.log(req.query)  //  {email:'', password:''}
+    res.send({ 'name': "Himanshu", 'last_name': "Sharma" })
+})
+
+// HOW TO GET THE ALL PARAMS ID 
+app.get('/users/L:userId/:bookingId', (req, res) => {
+    console.log(req.params) // return {userId:1,bookingId:34}
+    res.send({ 'name': "Himanshu", 'last_name': "Sharma" })
+})
